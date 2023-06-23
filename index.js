9f2543560c06bcc830019e4698c71f892e269411
Jonathan Mejia <jmejiag25@gmail.com>
2023-06-23 16:34:30 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import conectarDB from "./config/db.js";
+import veterinarioRoutes from "./routes/veterinarioRoutes.js";
+
+const app = express();
+conectarDB();
+
+console.log(process.env.MOMGO_URI);
+
+app.use("/api/veterinarios", veterinarioRoutes);
+app.use("/api/veterinarios/login", veterinarioRoutes);
+
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Servidor funcionando por: ${PORT}`);
+});

eb472d1db4fa5d56ee0262e51a8856d30913d61b
Jonathan Mejia <jmejiag25@gmail.com>
2023-06-27 06:50:46 -0600

    helpers y tokens

--- controllers/veterinarioControllers.js
+++ controllers/veterinarioControllers.js
@@ -1,11 +1,27 @@
-const registrar = (req, res) => {
+import Veterinario from "../models/Veterinario.js";
 
-  console.log(req.body);
-  res.send({msg: "Desde API/VETERINARIOS"});
+const registrar = async (req, res) => {
+  const { email } = req.body; 
+  //verificamos en la BD si existe el usuario 
+  const existeUsuario = await Veterinario.findOne({ email });
+
+  if (existeUsuario) {
+    const error = new Error("Usuario ya registrado");
+    return res.status(400).json({ msg: error.message });
+  }
+
+  try {
+    const veterinario = new Veterinario(req.body);
+    const veterinarioGuardado = await veterinario.save();
+
+    res.send({ msg: "Registrando usuario" });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const perfil = (req, res) => {
-  res.send({url: "desde la api/veterinarios/perfil"});
+  res.send({ url: "desde la api/veterinarios/perfil" });
 };
 
 export { registrar, perfil };

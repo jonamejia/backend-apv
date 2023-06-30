1871894ca227279dff1852233d2c94c5782227a6
Jonathan Mejia <jmejiag25@gmail.com>
2023-06-29 21:02:44 -0600

    agregando JWT

--- controllers/veterinarioControllers.js
+++ controllers/veterinarioControllers.js
@@ -1,4 +1,5 @@
 import Veterinario from "../models/Veterinario.js";
+import generarJWT from "../helpers/generarJWT.js";
 
 const registrar = async (req, res) => {
   const { email } = req.body;
@@ -47,16 +48,29 @@ const confirmar = async (req, res) => {
 };
 
 const autenticar = async (req, res) => {
-  const { email } = req.body;
+  const { email, password } = req.body;
 
   const usuario = await Veterinario.findOne({ email });
 
-  if(usuario) {
-    console.log("Si existe");
-  } else  {
-    res.status(403).json({msg: "EL usuario no existe"});
+  if (!usuario) {
+    const error = new Error("El usuario no existe");
+    return res.status(404).json({ msg: error.message });
   }
-  res.json({ msg: "Autenticando" });
+
+  if (!usuario.confirmado) {
+    const error = new Error("Tu cuenta no ha sido confirmada");
+    return res.status(403).json({ msg: error.message });
+  }
+  //Revisar el password
+
+  if (await usuario.comprobarPassword(password)) {
+    // Autenticar
+
+    res.json({ token: generarJWT(usuario.id) });
+  } else {
+    const error = new Error("Tu password es incorrecto");
+    return res.status(403).json({ msg: error.message });
+  }
 };
 
 export { registrar, perfil, confirmar, autenticar };
